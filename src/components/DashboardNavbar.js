f16a0de54346fe5e1a0d7e563700dca5b9578a1f
montasim <montasimmamun@gmail.com>
2022-10-16 16:04:30 -0700

    dashboard navbar icons, links

--- src/components/DashboardNavbar.js
+++ src/components/DashboardNavbar.js
@@ -1,47 +1,74 @@
 import React from 'react';
+import { BsArrowsFullscreen, BsBell } from 'react-icons/bs';
+import { CgMenuGridR, CgMenuGridO } from 'react-icons/cg';
+import { FiSettings } from 'react-icons/fi';
+import { GiRiceCooker } from 'react-icons/gi';
 import { HiMenuAlt3 } from 'react-icons/hi';
+import { RiListUnordered } from 'react-icons/ri';
+import { TbLanguage } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 
 const DashboardNavbar = () => {
-  const navbar = [
+  const navbarMiddle = [
     {
-      name: 'Home',
-      link: '/home'
+      name: 'Order List',
+      link: '/dashboard/order-list',
+      icon: <RiListUnordered />
     },
     {
-      name: 'Reservation',
-      link: '/reservation'
+      name: 'Kitchen Dashboard',
+      link: '/dashboard/kitchen-dashboard',
+      icon: <GiRiceCooker />
     },
     {
-      name: 'Menu',
-      link: '/menu'
-    },
-    {
-      name: 'About Us',
-      link: '/about-Us'
-    },
-    {
-      name: 'Contact Us',
-      link: '/contact-us'
-    },
-    {
-      name: 'Login',
-      link: '/login'
+      name: 'Counter Dashboard',
+      link: '/dashboard/counter-dashboard',
+      icon: <CgMenuGridR />
     }
   ];
 
-  const navbarMenu = navbar.map(navbarItems => <li key={navbarItems.link}><Link to={navbarItems.link}>{navbarItems.name}</Link></li>);
+  const navbarRight = [
+    {
+      icon: <BsArrowsFullscreen />,
+      link: ''
+    },
+    {
+      icon: <BsBell />,
+      link: '/dashboard/notification'
+    },
+    {
+      icon: <FiSettings />,
+      link: '/dashboard/settings'
+    },
+    {
+      icon: <TbLanguage />,
+      link: '/dashboard/select-language'
+    }
+  ];
 
   return (
-    <div className="navbar bg-base-100 border-b">
+    <div className="navbar bg-base-100">
       <div className="navbar-start">
-        <a className="btn btn-ghost normal-case text-xl">Bhojon</a>
+        <a className="btn btn-ghost normal-case text-2xl"><CgMenuGridO /></a>
+      </div>
+
+      <div className='navbar-middle'>
+        <ul className="menu menu-horizontal lg:flex gap-x-2 hidden">
+          {
+            navbarMiddle.map(navbarItems => <li key={navbarItems.link} className='border rounded-md'>
+              <Link to={navbarItems.link}>
+                <span>{navbarItems?.icon}</span>
+                {navbarItems.name}
+              </Link>
+            </li>)
+          }
+        </ul>
       </div>
 
       <div className="navbar-end">
-        <ul className="menu menu-horizontal p-0 lg:flex hidden">
+        <ul className="menu menu-horizontal lg:flex gap-x-2 hidden">
           {
-            navbarMenu
+            navbarRight.map(navbarItems => <li key={navbarItems.link} className='bg-gray-200 rounded-md'><Link to={navbarItems.link}>{navbarItems.icon}</Link></li>)
           }
         </ul>
 
@@ -51,7 +78,9 @@ const DashboardNavbar = () => {
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content shadow bg-base-100 rounded-box">
             {
-              navbarMenu
+              navbarRight.map(navbarItems => <li key={navbarItems.link}>
+                <Link to={navbarItems.link}>{navbarItems.name}</Link>
+              </li>)
             }
           </ul>
         </div>
