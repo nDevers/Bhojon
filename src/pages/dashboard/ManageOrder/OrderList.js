7838ec9970f9075b0da3f5e6a51121c47d57279e
montasim <montasimmamun@gmail.com>
2022-10-16 22:18:09 -0700

    order list page design

--- src/pages/dashboard/ManageOrder/OrderList.js
+++ src/pages/dashboard/ManageOrder/OrderList.js
@@ -1,9 +1,310 @@
 import React from 'react';
 
 const OrderList = () => {
+    const summaryData = [
+        {
+            name: 'Lifetime Orders',
+            value: 1000
+        },
+        {
+            name: 'Today Orders',
+            value: 100
+        },
+        {
+            name: 'Today Sell',
+            value: 80
+        },
+        {
+            name: 'Total Customer',
+            value: 8000
+        },
+        {
+            name: 'Total Delivered',
+            value: 6000
+        },
+        {
+            name: 'Total Reservation',
+            value: 90
+        }
+    ];
+
+    const latestSummary = [
+        {
+            title: 'Latest Order',
+            data: [
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+
+            ]
+        },
+        {
+            title: 'Delivered Order',
+            data: [
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+
+            ]
+        },
+        {
+            title: 'Cancel Order',
+            data: [
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+                {
+                    name: 'Kamal Hassan',
+                    phone: '0171742241476',
+                    order: 8787,
+                    table: '',
+                    time: '23:20:04'
+                },
+
+            ]
+        }
+    ];
+
     return (
-        <div>
-            Order List
+        <div className='border rounded-md'>
+            <div className='flex items-center justify-end border-b pb-4 mb-4'>
+                <h2>Order list</h2>
+
+                <div className='flex items-center gap-x-4'>
+                    <div className="form-control">
+                        <label className="input-group input-group-xs">
+                            <span>From</span>
+                            <input type="text" placeholder="Start Date" className="input input-bordered input-xs" />
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="input-group input-group-xs">
+                            <span>To</span>
+                            <input type="text" placeholder="End Date" className="input input-bordered input-xs" />
+                        </label>
+                    </div>
+
+                    <button className="btn btn-xs">Search</button>
+                    <button className="btn btn-xs">Reset</button>
+                </div>
+            </div>
+
+
         </div>
     );
 };
