6f596d4dea535136f54567a72d1535ccf08736eb
montasim <montasimmamun@gmail.com>
2022-10-19 14:55:35 +0600

    Breadcrumbs components #13

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -15,6 +15,7 @@ import { SlBriefcase } from 'react-icons/sl';
 import { TbLanguage } from 'react-icons/tb';
 import DefaultUserImage from '../../assets/images/defaultUser.png';
 import Pagination from '../../components/Pagination';
+import Breadcrumbs from '../../components/Breadcrumbs';
 
 const Dashboard = () => {
     const sideMenus = [
@@ -249,62 +250,7 @@ const Dashboard = () => {
 
                     <main className="scroll-smooth scrollbar-thin scrollbar-thumb-gray-400 overflow-x-auto">
                         <div className='m-4'>
-                            <div className="text-sm breadcrumbs mb-4">
-                                <ul>
-                                    <li>
-                                        <a>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                className="w-4 h-4 mr-2 stroke-current"
-                                            >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    strokeWidth="2"
-                                                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                                                ></path>
-                                            </svg>
-                                            Home
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                className="w-4 h-4 mr-2 stroke-current"
-                                            >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    strokeWidth="2"
-                                                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                                                ></path>
-                                            </svg>
-                                            Documents
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            className="w-4 h-4 mr-2 stroke-current"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                                            ></path>
-                                        </svg>
-                                        Add Document
-                                    </li>
-                                </ul>
-                            </div>
+                            <Breadcrumbs />
 
                             <Outlet />
 
