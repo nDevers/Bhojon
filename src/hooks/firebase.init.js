19ea36c2594c5bbe354795058aba6a8f69040985
MOHAMMAD MONTASIM -AL- MAMUN SHUVO <mmamun@mymedicalhub.com>
2022-10-19 13:11:13 +0600

    firebase auth init

---
+++ src/hooks/firebase.init.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAH_zOzNt-H5T8gQxvne0uShaJ6TlCiwPQ",
+  authDomain: "bhojon-39d34.firebaseapp.com",
+  projectId: "bhojon-39d34",
+  storageBucket: "bhojon-39d34.appspot.com",
+  messagingSenderId: "216986720963",
+  appId: "1:216986720963:web:f3b0520acac2e76deae076",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// initialize auth
+const auth = getAuth(app);
+
+// exporting auth for later use
+export default auth;
