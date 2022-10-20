785a3ac8ec18196e1a0c0839d26cdfc537076cb5
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-20 19:07:22 +0600

    add user page design
    Co-Authored-By: AbidHasanPiash <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const Select = ({ selectTitle = 'Select Title', selectOptions = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5'] }) => {
-    return (
-        <div className="grid grid-cols-2 items-center w-3/6">
-            <h3>{selectTitle}</h3>
-            <select className="select select-bordered select-xs md:select-sm lg:select-sm w-full max-w-xs">
-                <option disabled>Select your Choice</option>
-                {
-                    selectOptions.map(selectOption => <option key={selectOption}>{selectOption}</option>)
-                }
-            </select>
-        </div>
-    );
+const Select = ({
+  selectTitle = "Select Title",
+  selectOptions = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"],
+}) => {
+  return (
+    <div className="grid grid-cols-2 items-center w-full md:4/6 lg:w-3/6">
+      <h3 className="font-medium">{selectTitle}</h3>
+      <select className="select select-bordered select-xs md:select-sm lg:select-sm w-full max-w-xs">
+        <option disabled>Select your Choice</option>
+        {selectOptions.map((selectOption) => (
+          <option key={selectOption}>{selectOption}</option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 export default Select;
--- src/pages/dashboard/UserManagement/AddUser.js
+++ src/pages/dashboard/UserManagement/AddUser.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
+import FileInput from "../../../components/FileInput";
+import Input from "../../../components/Input";
+import Select from "../../../components/Select";
 
 const AddUser = () => {
-    return (
-        <div>
-            AddUser
+  return (
+    <div>
+      <h3 className="my-6 md:my-8 lg:my-10 pl-2">
+        This section is use Only for Store Management.
+      </h3>
+      <div className="border rounded-md">
+        <h3 className="uppercase font-semibold p-2 md:p-3 lg:p-3">Add User</h3>
+        <div className="border-t">
+          <div className="flex flex-col gap-5 items-center justify-center m-4 md:m-6 lg:m-10">
+            <Input
+              inputTitle="First Name"
+              inputType="text"
+              inputPlaceHolder="First Name"
+            />
+            <Input
+              inputTitle="Last Name"
+              inputType="text"
+              inputPlaceHolder="Last Name"
+            />
+            <FileInput fileInputTitle="Profile Picture" />
+            <Input
+              inputTitle="Email Address"
+              inputType="email"
+              inputPlaceHolder="company@email.com"
+            />
+            <Input
+              inputTitle="Password"
+              inputType="password"
+              inputPlaceHolder="Password"
+            />
+            <Input
+              inputTitle="Confirm Password"
+              inputType="password"
+              inputPlaceHolder="Confirm Password"
+            />
+            <Input
+              inputTitle="Phone"
+              inputType="tel"
+              inputPlaceHolder="+8801700000000"
+            />
+            <Input
+              inputTitle="Address"
+              inputType="text"
+              inputPlaceHolder="Address"
+            />
+            <Select
+              selectTitle="Customer Type"
+              selectOptions={["Active", "Inactive"]}
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddUser;
