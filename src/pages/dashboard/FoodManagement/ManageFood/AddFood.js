918cc4c49eec1a0b6da159ce4a4f39f760a90dfc
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-20 20:16:12 +0600

    fixed food page bug
    Co-Authored-By: AbidHasanPiash <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/pages/dashboard/FoodManagement/ManageFood/AddFood.js
+++ src/pages/dashboard/FoodManagement/ManageFood/AddFood.js
@@ -4,31 +4,50 @@ import FileInput from "../../../../components/FileInput";
 import Input from "../../../../components/Input";
 import Select from "../../../../components/Select";
 import TextArea from "../../../../components/TextArea";
+import Toggle from "../../../../components/Toggle";
 
 const AddFood = () => {
   return (
     <div className="my-10">
       <div className="flex flex-col gap-5 items-center justify-center p-2 m-2">
-        <Input inputTitle='Food Name' inputType='text' inputPlaceHolder='Food Name' />
-        <FileInput fileInputTitle='Food Image' />
-        <Select selectTitle='Category' selectOptions={['Desert', 'Drinks', 'Soup']} />
-        <Select selectTitle='Menu' selectOptions={['Breakfast', 'Lunch', 'Dinner']} />
-        <Select selectTitle='Select Kitchen' selectOptions={['Thai', 'Mexican', 'American']} />
-        <Input inputTitle='Components' inputType='text' inputPlaceHolder='Components' />
-        <Input inputTitle='Notes' inputType='text' inputPlaceHolder='Notes' />
-        <TextArea textAreaTitle='Description' textAreaPlaceHolder='Description' />
-        <Input inputTitle='Vat' inputType='number' inputPlaceHolder='Vat' />
-        <Input inputTitle='Cooking Time' inputType='time' />
-        <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>View Food</h3>
-          <div className='flex justify-end'>
-            <input type="checkbox" className="toggle" />
-          </div>
-        </div>
+        <Input
+          inputTitle="Food Name"
+          inputType="text"
+          inputPlaceHolder="Food Name"
+        />
+        <FileInput fileInputTitle="Food Image" />
+        <Select
+          selectTitle="Category"
+          selectOptions={["Desert", "Drinks", "Soup"]}
+        />
+        <Select
+          selectTitle="Menu"
+          selectOptions={["Breakfast", "Lunch", "Dinner"]}
+        />
+        <Select
+          selectTitle="Select Kitchen"
+          selectOptions={["Thai", "Mexican", "American"]}
+        />
+        <Input
+          inputTitle="Components"
+          inputType="text"
+          inputPlaceHolder="Components"
+        />
+        <Input inputTitle="Notes" inputType="text" inputPlaceHolder="Notes" />
+        <TextArea
+          textAreaTitle="Description"
+          textAreaPlaceHolder="Description"
+        />
+        <Input inputTitle="Vat" inputType="number" inputPlaceHolder="Vat" />
+        <Input inputTitle="Cooking Time" inputType="time" />
+        <Toggle toggleTitle="View Food" toggleDefaultValue="checked " />
         <div className="grid items-center justify-end w-3/6">
           <div className="flex justify-end gap-x-2">
-            <Button customClass='btn btn-sm btn-error text-xs text-white' buttonTitle='Reset' />
-            <Button buttonTitle='Save' />
+            <Button
+              customClass="btn btn-sm btn-error text-xs text-white"
+              buttonTitle="Reset"
+            />
+            <Button buttonTitle="Save" />
           </div>
         </div>
       </div>
