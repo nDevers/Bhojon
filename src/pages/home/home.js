87f4bfe9a2663da2d5b9b6e99f9dba48f2ee006e
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-20 17:50:08 +0600

    Demo loading spinner
    Co-Authored-By: AbidHasanPiash <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
-import Hero from './Hero';
-import Features from './Features';
+import React from "react";
+import { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar";
+import Hero from "./Hero";
+import Features from "./Features";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <Features />
-        </div>
-    );
+  const [loading, setLoading] = useState(true);
+
+  const onLoadEffect = () => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  };
+
+  useEffect(onLoadEffect, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Features />
+    </div>
+  );
 };
 
 export default Home;
