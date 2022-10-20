077f559ca58d940ba89e4b8085b93a1f1ef7b880
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-20 23:12:00 +0600

    add food

--- src/pages/dashboard/FoodManagement/ManageFood/AddFood.js
+++ src/pages/dashboard/FoodManagement/ManageFood/AddFood.js
@@ -1,57 +1,34 @@
 import React from "react";
-import { AiFillInfoCircle } from "react-icons/ai";
+import Button from "../../../../components/Button";
+import FileInput from "../../../../components/FileInput";
+import Input from "../../../../components/Input";
+import Select from "../../../../components/Select";
+import TextArea from "../../../../components/TextArea";
 
 const AddFood = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-5 justify-between p-2">
+    <div className="my-10">
+      <div className="flex flex-col gap-5 items-center justify-center p-2 m-2">
+        <Input inputTitle='Food Name' inputType='text' inputPlaceHolder='Food Name' />
+        <FileInput fileInputTitle='Food Image' />
+        <Select selectTitle='Category' selectOptions={['Desert', 'Drinks', 'Soup']} />
+        <Select selectTitle='Menu' selectOptions={['Breakfast', 'Lunch', 'Dinner']} />
+        <Select selectTitle='Select Kitchen' selectOptions={['Thai', 'Mexican', 'American']} />
+        <Input inputTitle='Components' inputType='text' inputPlaceHolder='Components' />
+        <Input inputTitle='Notes' inputType='text' inputPlaceHolder='Notes' />
+        <TextArea textAreaTitle='Description' textAreaPlaceHolder='Description' />
+        <Input inputTitle='Vat' inputType='number' inputPlaceHolder='Vat' />
+        <Input inputTitle='Cooking Time' inputType='time' />
         <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>Category Name</h3>
-          <input
-            type="text"
-            placeholder="Category Name"
-            className="input input-bordered input-xs md:input-sm lg:input-sm w-full max-w-xs"
-          />
-        </div>
-        <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>Offer Start Date</h3>
-          <input
-            type="date"
-            placeholder="Offer Start Date"
-            className="input input-bordered input-xs md:input-sm lg:input-sm w-full max-w-xs"
-          />
-        </div>
-        <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>Offer End Date</h3>
-          <input
-            type="date"
-            placeholder="Offer End Date"
-            className="input input-bordered input-xs md:input-sm lg:input-sm w-full max-w-xs"
-          />
-        </div>
-        <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>Parent Category</h3>
-          <select className="select select-bordered select-xs md:select-sm lg:select-sm w-full max-w-xs">
-            <option disabled selected>
-              Desert
-            </option>
-            <option>Drinks</option>
-            <option>Soup</option>
-          </select>
-        </div>
-        <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>View Category</h3>
-          <div className="flex justify-end">
+          <h3>View Food</h3>
+          <div className='flex justify-end'>
             <input type="checkbox" className="toggle" />
           </div>
         </div>
-        <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>Image</h3>
-          <div className="flex items-center gap-x-4">
-            <input type="file" id="myfile" name="myfile" />
-            <button className="p-1 text-cyan-400 border-0">
-              <AiFillInfoCircle />
-            </button>
+        <div className="grid items-center justify-end w-3/6">
+          <div className="flex justify-end gap-x-2">
+            <Button customClass='btn btn-sm btn-error text-xs text-white' buttonTitle='Reset' />
+            <Button buttonTitle='Save' />
           </div>
         </div>
       </div>
