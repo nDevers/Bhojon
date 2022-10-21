4e60d8cdd33b70938386667c81cf3c46117a13d8
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-21 15:27:01 +0600

    add-purchase page
    Co-Authored-By: AbidHasanPiash <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,10 +4,11 @@ const Input = ({
   inputTitle = "Input Title",
   inputType = "text",
   inputPlaceHolder = "Input Placeholder",
+  inputComponentsCustomClass = "grid grid-cols-2 items-center w-full md:4/6 lg:w-3/6",
   inputCustomClass = "input input-bordered input-xs md:input-sm lg:input-sm w-full max-w-xs",
 }) => {
   return (
-    <div className="grid grid-cols-2 items-center w-full md:4/6 lg:w-3/6">
+    <div className={inputComponentsCustomClass}>
       <h3 className="text-sm md:text-text-[16px] lg:text-[16px] font-medium">{inputTitle}</h3>
       <input
         type={inputType}
--- src/components/Select.js
+++ src/components/Select.js
@@ -3,9 +3,10 @@ import React from "react";
 const Select = ({
   selectTitle = "Select Title",
   selectOptions = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"],
+  selectComponentsCustomClass = "grid grid-cols-2 items-center w-full md:4/6 lg:w-3/6",
 }) => {
   return (
-    <div className="grid grid-cols-2 items-center w-full md:4/6 lg:w-3/6">
+    <div className={selectComponentsCustomClass}>
       <h3 className="text-sm md:text-text-[16px] lg:text-[16px] font-medium">{selectTitle}</h3>
       <select className="select select-bordered select-xs md:select-sm lg:select-sm w-full max-w-xs">
         <option disabled>Select your Choice</option>
