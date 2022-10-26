b8b8af419974ad5cf22acf2ecfbf68e6ed5c4beb
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-26 17:21:55 +0600

    authentication folder is separated

---
+++ src/pages/Authentication/VerifyEmail.js
@@ -0,0 +1,11 @@
+import React from "react";
+import useWebsiteTitle from "../../hooks/useWebsiteTitle";
+
+const VerifyEmail = () => {
+  // set website title
+  useWebsiteTitle("Bhojon | Verify Email");
+
+  return <div>Verify Email</div>;
+};
+
+export default VerifyEmail;
