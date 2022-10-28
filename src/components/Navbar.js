13883bba3daa6b9452b2dd25022af111475f854a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-28 14:59:21 +0600

    navbar design
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,86 @@
 import React from "react";
+import { BsSearch, BsCart3 } from "react-icons/bs";
+import { HiMenuAlt3 } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 
-function Navbar() {
-  return (
-    <div className="fixed top-0 right-0 z-50 w-screen mx-6 mt-3 mb-16">
-      <div className="flex justify-between items-center">
-        <Logo customclassName="flex gap-x-4 items-center justify-center text-2xl text-center text-stone-100 font-semibold uppercase p-4 ml-12 cursor-pointer" />
+const Navbar = () => {
+  const navbar = [
+    {
+      name: "Home",
+      link: "/home",
+    },
+    {
+      name: "Reservation",
+      link: "/reservation",
+    },
+    {
+      name: "Menu",
+      link: "/menu",
+    },
+    {
+      name: "About Us",
+      link: "/about-Us",
+    },
+    {
+      name: "Contact Us",
+      link: "/contact-us",
+    },
+    {
+      name: "Login",
+      link: "/login",
+    },
+    {
+      name: <BsSearch />,
+      link: "/search",
+    },
+    {
+      name: <BsCart3 />,
+      link: "/cart",
+    },
+  ];
 
-        <ul className="flex gap-x-4 font-medium">
-          <li className="text-lg cursor-pointer">
-            <Link to="hero" duration={500}>
-              Home
-            </Link>
-          </li>
-          <li className="text-lg cursor-pointer">
-            <Link to="features" duration={500}>
-              Features
-            </Link>
-          </li>
-          <li className="text-lg cursor-pointer">
-            <Link to="hero" duration={500}>
-              Services
-            </Link>
-          </li>
-          <li className="text-lg cursor-pointer">
-            <Link to="hero" duration={500}>
-              About
-            </Link>
-          </li>
-          <li className="text-lg cursor-pointer">
-            <Link to="hero" duration={500}>
-              Contact
-            </Link>
+  const navbarMenu = navbar.map((navbarItems) => (
+    <li key={navbarItems.link}>
+      <Link to={navbarItems.link}>{navbarItems.name}</Link>
+    </li>
+  ));
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          <Logo />
+        </Link>
+      </div>
+
+      <div className="navbar-end">
+        <ul className="menu menu-horizontal p-0 lg:flex hidden">
+          {navbarMenu}
+
+          <li>
+            <Link to="/">Logout</Link>
           </li>
         </ul>
+
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <HiMenuAlt3 className="text-2xl" />
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navbarMenu}
+
+            <li>
+              <Link to="/">Logout</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
