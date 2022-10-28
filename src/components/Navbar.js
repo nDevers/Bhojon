1bec7cba94443a9d7c1fb423f6f99278204ee858
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-28 15:42:30 +0600

    navbar design
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Swal from "sweetalert2";
+import { toast } from "react-toastify";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 import { BsSearch, BsCart3 } from "react-icons/bs";
-import { HiMenuAlt3 } from "react-icons/hi";
+import { HiMenuAlt3, HiOutlineLogout } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
+import auth from "../hooks/firebase.init";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Navbar = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   const navbar = [
     {
       name: "Home",
@@ -28,7 +36,7 @@ const Navbar = () => {
     },
     {
       name: "Login",
-      link: "/login",
+      link: "/authentication/login",
     },
     {
       name: <BsSearch />,
@@ -46,6 +54,45 @@ const Navbar = () => {
     </li>
   ));
 
+  const useSignOut = user && (
+    <li>
+      <Link
+        onClick={() =>
+          Swal.fire({
+            icon: "warning",
+            title: "Are you sure want to sign out?",
+            showCancelButton: true,
+            confirmButtonText: "Yes",
+            customClass: {
+              actions: "my-actions",
+              cancelButton: "order-2 right-gap",
+              confirmButton: "order-1",
+            },
+          }).then((result) => {
+            if (result.isConfirmed) {
+              signOut(auth);
+
+              toast.success("See you soon!");
+            }
+          })
+        }
+      >
+        <Link>
+          <HiOutlineLogout />
+        </Link>
+      </Link>
+    </li>
+  );
+
+  loading && <LoadingSpinner />;
+
+  error &&
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: `${error}`,
+    });
+
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -55,12 +102,10 @@ const Navbar = () => {
       </div>
 
       <div className="navbar-end">
-        <ul className="menu menu-horizontal p-0 lg:flex hidden">
+        <ul className="menu menu-horizontal font-medium p-0 lg:flex hidden">
           {navbarMenu}
 
-          <li>
-            <Link to="/">Logout</Link>
-          </li>
+          {useSignOut}
         </ul>
 
         <div className="dropdown dropdown-end">
@@ -69,13 +114,38 @@ const Navbar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 font-medium p-2 shadow bg-base-100 rounded-box w-52"
           >
             {navbarMenu}
 
-            <li>
-              <Link to="/">Logout</Link>
-            </li>
+            {user && (
+              <Link
+                onClick={() =>
+                  Swal.fire({
+                    icon: "warning",
+                    title: "Are you sure want to sign out?",
+                    showCancelButton: true,
+                    confirmButtonText: "Yes",
+                    customClass: {
+                      actions: "my-actions",
+                      cancelButton: "order-2 right-gap",
+                      confirmButton: "order-1",
+                    },
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      signOut(auth);
+
+                      toast.success("See you soon!");
+                    }
+                  })
+                }
+                className="bg-gray-200 p-1 md:p-2 lg:p-2 rounded-md"
+              >
+                <Link>
+                  <HiOutlineLogout />
+                </Link>
+              </Link>
+            )}
           </ul>
         </div>
       </div>
