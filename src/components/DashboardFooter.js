d1a2ad797e4c737aac6db10eca9f9705a31804c4
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-29 14:37:49 +0600

    class is className
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/components/DashboardFooter.js
+++ src/components/DashboardFooter.js
@@ -5,12 +5,12 @@ const DashboardFooter = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer aria-label="Site Footer" class="bg-gray-50">
-      <div class="px-4 py-3 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between">
+    <footer aria-label="Site Footer" className="bg-gray-50">
+      <div className="px-4 py-3 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between">
           <Logo />
 
-          <p class="text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+          <p className="text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
             Copyright &copy; {currentYear}. All rights reserved.
           </p>
         </div>
