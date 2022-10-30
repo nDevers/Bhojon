094f199d9151181694589b741197c937ccc9db09
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-30 22:28:37 +0600

    settings page design for mobile device
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,9 +1,6 @@
 import React from "react";
 
-const Toggle = ({
-  toggleTitle = "Toggle Title",
-  toggleDefaultValue = "checked",
-}) => {
+const Toggle = ({ toggleTitle = "Toggle Title" }) => {
   return (
     <div className="grid grid-cols-2 items-center justify-end w-full md:3/6 lg:w-3/6">
       <h3 className="text-sm md:text-text-[16px] lg:text-[16px] font-medium">{toggleTitle}</h3>
