13f0f0b68b0258425904f810df6648bf9fb8fb0b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-30 19:43:00 +0600

    fixed loading spinner time
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Swal from "sweetalert2";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import ScrollToTop from "../../components/ScrollToTop";
 import Footer from "../../components/Footer";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../hooks/firebase.init";
 
 const Home = () => {
+  const [user, loading, error] = useAuthState(auth);
+
+  loading && <LoadingSpinner />;
+
+  error &&
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: `${error}`,
+    });
+
   return (
     <div>
       <Navbar />
@@ -18,7 +33,7 @@ const Home = () => {
       {/* scroll to top button */}
       <ScrollToTop />
     </div>
-  );
+  )
 };
 
 export default Home;
