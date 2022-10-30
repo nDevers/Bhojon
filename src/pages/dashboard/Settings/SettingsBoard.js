b7c34a792b87ca75880db28186b6de455eb66a95
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-30 22:34:59 +0600

    settings board page mobile design
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/pages/dashboard/Settings/SettingsBoard.js
+++ src/pages/dashboard/Settings/SettingsBoard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiFillInfoCircle } from "react-icons/ai";
+import FileInput from "../../../components/FileInput";
 import Input from "../../../components/Input";
 import useWebsiteTitle from "../../../hooks/useWebsiteTitle";
 
@@ -11,24 +11,8 @@ const SettingsBoard = () => {
     <div className="my-10">
       <div className="flex flex-col gap-5 items-center justify-center p-2 m-2">
         <Input inputTitle='Website Name' inputType='text' inputPlaceHolder='Website Name' />
-        <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>Website Logo</h3>
-          <div className="flex items-center gap-x-4">
-            <input type="file" id="myfile" name="myfile" />
-            <button className="p-1 text-cyan-400 border-0">
-              <AiFillInfoCircle />
-            </button>
-          </div>
-        </div>
-        <div className="grid grid-cols-2 items-center w-3/6">
-          <h3>Website Favicon</h3>
-          <div className="flex items-center gap-x-4">
-            <input type="file" id="myfile" name="myfile" />
-            <button className="p-1 text-cyan-400 border-0">
-              <AiFillInfoCircle />
-            </button>
-          </div>
-        </div>
+        <FileInput fileInputTitle="Website Logo" />
+        <FileInput fileInputTitle="Website Favicon" />
         <Input inputTitle='Address' inputType='text' inputPlaceHolder='Address' />
         <Input inputTitle='Email Address' inputType='email' inputPlaceHolder='company@email.com' />
         <Input inputTitle='Phone' inputType='tel' inputPlaceHolder='+8801700000000' />
