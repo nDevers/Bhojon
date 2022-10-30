2e3e61b07eb4717d692cc3f8bb0dac18cd31d722
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-10-30 23:44:19 +0600

    supported file type text
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -12,7 +12,17 @@ const FileInput = ({
       <div className="flex items-center gap-x-4">
         <input type="file" id="myfile" name="myfile" />
         <button className={`p-1 ${fileButtonColor} border-0`}>
-          <AiFillInfoCircle />
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-circle btn-ghost btn-xs text-warning">
+              <AiFillInfoCircle className="text-lg" />
+            </label>
+            <div tabIndex={0} className="card compact dropdown-content shadow bg-base-100 rounded-box w-64">
+              <div className="card-body text-black text-center">
+                <h2 className="text-xl text-warning">PNG and JPEG</h2>
+                <p>file types are supported only.</p>
+              </div>
+            </div>
+          </div>
         </button>
       </div>
     </div>
