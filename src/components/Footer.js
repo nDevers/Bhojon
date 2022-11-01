24504a5485df9db94b8d198eb72556113b8027fd
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-11-01 18:31:41 +0600

    footer code optimized
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
-import { AiFillFacebook, AiFillGithub, AiFillLinkedin, AiOutlinePlus } from "react-icons/ai";
+import {
+  AiFillFacebook,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiOutlinePlus,
+} from "react-icons/ai";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 
@@ -24,13 +29,14 @@ const Footer = () => {
   const checkTemporaryEmailAddress = () => {
     watch("email") &&
       fetch(`https://www.disify.com/api/email/${watch("email")}`)
-        .then(response => response.json())
-        .then(isTemporaryEmail => {
+        .then((response) => response.json())
+        .then((isTemporaryEmail) => {
           if (isTemporaryEmail?.disposable === true) {
-            temporaryEmailAddressMatchedText = 'Sorry temporary email address is not allowed';
+            temporaryEmailAddressMatchedText =
+              "Sorry temporary email address is not allowed";
           }
         });
-  }
+  };
 
   // sent email request
   const onSubmit = async (data, errors) => {
@@ -41,12 +47,11 @@ const Footer = () => {
         title: "Error",
         text: `${temporaryEmailAddressMatchedText}`,
       });
-    }
-    else {
+    } else {
       Swal.fire({
         icon: "success",
         title: "Email sent successfully",
-        text: 'We will contact you soon',
+        text: "We will contact you soon",
         confirmButtonText: "Yes",
         customClass: {
           actions: "my-actions",
@@ -56,29 +61,106 @@ const Footer = () => {
       }).then((result) => {
         if (result.isConfirmed) {
         }
-      })
+      });
     }
   };
 
+  const footerLinks = [
+    {
+      category: "About Us",
+      links: [
+        {
+          name: "Company History",
+          link: "/company-history",
+        },
+        {
+          name: "Meet the Team",
+          link: "/meet-the-team",
+        },
+        {
+          name: "Employee Handbook",
+          link: "/employee-handbook",
+        },
+        {
+          name: "Careers",
+          link: "/careers",
+        },
+      ],
+    },
+    {
+      category: "Our Services",
+      links: [
+        {
+          name: "Web Development",
+          link: "/web-development",
+        },
+        {
+          name: "Web Design",
+          link: "/web-design",
+        },
+        {
+          name: "Marketing",
+          link: "/marketing",
+        },
+        {
+          name: "Google Ads",
+          link: "//google-ads",
+        },
+      ],
+    },
+    {
+      category: "Helpful Links",
+      links: [
+        {
+          name: "FAQs",
+          link: "/faqs",
+        },
+        {
+          name: "Support",
+          link: "/support",
+        },
+        {
+          name: "Live Chat",
+          link: "/live-chat",
+        },
+      ],
+    },
+  ];
+
+  const otherLinks = [
+    {
+      name: "Privacy Policy",
+      link: "/privacy-policy",
+    },
+    {
+      name: "Terms & Conditions",
+      link: "/terms-and-conditions",
+    },
+    {
+      name: "Cookie Policy",
+      link: "/cookie-policy",
+    },
+  ];
+
   const socialMediaLinks = [
     {
-      "id": 1,
-      "name": "LinkedIn",
-      "link": "https://www.linkedin.com/in/montasim",
-      "icon": <AiFillLinkedin />
+      id: 1,
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/montasim",
+      icon: <AiFillLinkedin />,
     },
     {
-      "id": 2,
-      "name": "GitHub",
-      "link": "https://github.com/montasim",
-      "icon": <AiFillGithub />
+      id: 2,
+      name: "GitHub",
+      link: "https://github.com/montasim",
+      icon: <AiFillGithub />,
     },
     {
-      "id": 3,
-      "name": "Facebook",
-      "link": "https://www.facebook.com/montasimmamun/",
-      "icon": <AiFillFacebook />
-    }
+      id: 3,
+      name: "Facebook",
+      link: "https://www.facebook.com/montasimmamun/",
+      icon: <AiFillFacebook />,
+    },
   ];
 
   return (
@@ -99,7 +181,9 @@ const Footer = () => {
                 </h2>
 
                 <p className="mx-auto mt-4 max-w-md text-sm leading-relaxed text-gray-400 lg:mx-0">
-                  Here all software, apps, themes, plugins, are our own property. Therefore copying or reselling is strictly prohibited.
+                  Here all software, apps, themes, plugins, are our own
+                  property. Therefore copying or reselling is strictly
+                  prohibited.
                 </p>
               </div>
 
@@ -137,12 +221,11 @@ const Footer = () => {
                   </button>
                 </div>
 
-                {
-                  errors.email?.message &&
+                {errors.email?.message && (
                   <p role="alert" className="text-error text-sm mt-2 mx-4">
                     {errors.email?.message}
                   </p>
-                }
+                )}
 
                 <p role="alert" className="text-error text-sm mt-2 mx-4">
                   {temporaryEmailAddressMatchedText}
@@ -152,16 +235,19 @@ const Footer = () => {
 
             {/* social media links */}
             <div className="flex items-center justify-center md:justify-start lg:justify-start col-span-2 space-x-4 text-gray-300 lg:col-span-5 mt-10">
-              {
-                socialMediaLinks.map(socialMediaLink =>
-                  <a key={socialMediaLink.id} className="hover:text-error" href={socialMediaLink.link} target="_blank" rel="noreferrer">
-                    <span className="sr-only"> {socialMediaLink.name} </span>
+              {socialMediaLinks.map((socialMediaLink) => (
+                <a
+                  key={socialMediaLink.id}
+                  className="hover:text-error"
+                  href={socialMediaLink.link}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="sr-only"> {socialMediaLink.name} </span>
 
-                    <span className='text-2xl'>
-                      {socialMediaLink.icon}
-                    </span>
-                  </a>)
-              }
+                  <span className="text-2xl">{socialMediaLink.icon}</span>
+                </a>
+              ))}
             </div>
           </div>
 
@@ -171,169 +257,54 @@ const Footer = () => {
             </Link>
 
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 lg:mt-12">
-              <div className="text-center lg:text-left">
-                <p className="text-lg font-medium text-white">About Us</p>
+              {footerLinks?.map((footerLink) => (
+                <div
+                  className="text-center lg:text-left"
+                  key={footerLink?.category}
+                >
+                  <p className="text-lg font-medium text-white">
+                    {footerLink?.category}
+                  </p>
 
-                <nav aria-label="Footer About Nav" className="mt-4">
-                  <ul className="space-y-1.5 text-sm">
-                    {
-                      <li>
-                        <Link
-                          className="text-white transition hover:text-error hover:underline"
-                          to="/company-history"
-                        >
-                          Company History
-                        </Link>
-                      </li>
-                    }
-
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/meet-the-team"
-                      >
-                        Meet the Team
-                      </Link>
-                    </li>
-
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/employee-handbook"
-                      >
-                        Employee Handbook
-                      </Link>
-                    </li>
-
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/careers"
-                      >
-                        Careers
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-
-              <div className="text-center lg:text-left">
-                <p className="text-lg font-medium text-white">Our Services</p>
-
-                <nav aria-label="Footer Services Nav" className="mt-4">
-                  <ul className="space-y-1.5 text-sm">
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/web-development"
-                      >
-                        Web Development
-                      </Link>
-                    </li>
-
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/web-design"
-                      >
-                        Web Design
-                      </Link>
-                    </li>
-
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/marketing"
-                      >
-                        Marketing
-                      </Link>
-                    </li>
-
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/google-ads"
-                      >
-                        Google Ads
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-
-              <div className="text-center lg:text-left">
-                <p className="text-lg font-medium text-white">Helpful Links</p>
-
-                <nav aria-label="Footer Helpful Nav" className="mt-4">
-                  <ul className="space-y-1.5 text-sm">
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/faqs"
-                      >
-                        FAQs
-                      </Link>
-                    </li>
-
-                    <li>
-                      <Link
-                        className="text-white transition hover:text-error hover:underline"
-                        to="/support"
-                      >
-                        Support
-                      </Link>
-                    </li>
-
-                    <li>
-                      <Link
-                        className="group flex justify-center gap-1.5 lg:justify-start"
-                        to="/live-chat"
-                      >
-                        <span className="text-white transition group-hover:text-error hover:underline">
-                          Live Chat
-                        </span>
-
-                        <span className="relative flex h-2 w-2">
-                          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-teal-400 opacity-75"></span>
-                          <span className="relative inline-flex h-2 w-2 rounded-full bg-teal-500"></span>
-                        </span>
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
+                  <nav aria-label="Footer About Nav" className="mt-4">
+                    <ul className="space-y-1.5 text-sm">
+                      {footerLink?.links?.map((link) => (
+                        <li>
+                          <Link
+                            className="text-white transition hover:text-error hover:underline"
+                            to={link?.link}
+                          >
+                            {link?.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </nav>
+                </div>
+              ))}
             </div>
 
             <div className="mt-16 border-t border-gray-800 pt-8 text-sm text-white lg:mt-24">
               <p className="flex gap-x-4 items-center justify-center md:justify-start lg:justify-start md:text-center lg:text-left">
-                <Link
-                  className="inline-block text-white hover:underline transition hover:text-error"
-                  to="/privacy-policy"
-                >
-                  Privacy Policy
-                </Link>
+                {otherLinks?.map((otherLink) => (
+                  <>
+                    <Link
+                      key={otherLink?.name}
+                      className="inline-block text-white hover:underline transition hover:text-error"
+                      to={otherLink?.link}
+                    >
+                      {otherLink?.name}
+                    </Link>
 
-                <span>&middot;</span>
-
-                <Link
-                  className="inline-block text-white hover:underline transition hover:text-error"
-                  to="/terms-and-conditions"
-                >
-                  Terms & Conditions
-                </Link>
-
-                <span>&middot;</span>
-
-                <Link
-                  className="inline-block text-white hover:underline transition hover:text-error"
-                  to="/cookie-policy"
-                >
-                  Cookie Policy
-                </Link>
+                    <span>&middot;</span>
+                  </>
+                ))}
               </p>
 
               <p className="mt-4 text-center lg:text-left flex items-center justify-center md:justify-start lg:justify-start gap-3">
-                &copy; {currentYear} <Logo customClass='flex gap-x-2 items-center text-md text-center text-black-50 font-semibold font-mono uppercase' /> All rights reserved.
+                &copy; {currentYear}{" "}
+                <Logo customClass="flex gap-x-2 items-center text-md text-center text-black-50 font-semibold font-mono uppercase" />{" "}
+                All rights reserved.
               </p>
             </div>
           </div>
