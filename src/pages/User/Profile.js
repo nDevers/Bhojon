b5b737aba937b003ed3f2ad49eddcf0d4c232440
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-11-04 12:29:53 +0600

    profile margin padding
    Co-Authored-By: AႦιԃ Hαʂαɳ <60427103+AbidHasanPiash@users.noreply.github.com>

--- src/pages/User/Profile.js
+++ src/pages/User/Profile.js
@@ -6,44 +6,44 @@ import Input from "../../components/Input";
 import useWebsiteTitle from "../../hooks/useWebsiteTitle";
 
 const UserSettings = () => {
-    // set website title
-    useWebsiteTitle('User | Profile')
+  // set website title
+  useWebsiteTitle("User | Profile");
 
-    return (
-        <div className="mx-2 md:mx-32 lg:mx-40 h-screen">
-            <div className="m-2">
-                < Breadcrumbs />
-            </div >
-            <div className="border rounded-md">
-                <h3 className="uppercase font-semibold p-2">Profile</h3>
-                <div className="border-t">
-                    <div className="flex flex-col gap-5 items-center justify-center m-2 md:m-6 lg:m-10">
-                        <Input
-                            inputTitle="First Name"
-                            inputType="text"
-                            inputPlaceHolder="First Name"
-                        />
-                        <Input
-                            inputTitle="Last Name"
-                            inputType="text"
-                            inputPlaceHolder="Last Name"
-                        />
-                        <FileInput fileInputTitle="Profile Picture" />
-                        <Input
-                            inputTitle="Address"
-                            inputType="text"
-                            inputPlaceHolder="Address"
-                        />
-                        <div className="grid w-full md:w-3/6 lg:w-3/6">
-                            <div className="flex justify-end gap-x-2">
-                                <Button buttonTitle='Save' />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="mx-4 md:mx-32 lg:mx-40 h-screen">
+      <div className="m-2">
+        <Breadcrumbs />
+      </div>
+      <div className="border rounded-md">
+        <h3 className="uppercase font-semibold p-2">Profile</h3>
+        <div className="border-t">
+          <div className="flex flex-col gap-5 items-center justify-center m-3 md:m-6 lg:m-10">
+            <Input
+              inputTitle="First Name"
+              inputType="text"
+              inputPlaceHolder="First Name"
+            />
+            <Input
+              inputTitle="Last Name"
+              inputType="text"
+              inputPlaceHolder="Last Name"
+            />
+            <FileInput fileInputTitle="Profile Picture" />
+            <Input
+              inputTitle="Address"
+              inputType="text"
+              inputPlaceHolder="Address"
+            />
+            <div className="grid w-full md:w-3/6 lg:w-3/6">
+              <div className="flex justify-end gap-x-2">
+                <Button buttonTitle="Save" />
+              </div>
             </div>
-        </div >
-    );
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserSettings;
