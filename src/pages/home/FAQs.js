757e3b63728c3612da3237712aebc7ab84faf92b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-11-07 20:08:01 +0600

    faqs page

--- src/pages/home/FAQs.js
+++ src/pages/home/FAQs.js
@@ -1,7 +1,116 @@
 import React from "react";
+import faqsImage from "../../assets/svgs/faqs.svg";
 
 const FAQs = () => {
-  return <div>FAQs</div>;
+  return (
+    <section className="relative flex flex-wrap justify-between lg:h-screen lg:items-center">
+      <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
+        <div class="-my-8 divide-y divide-gray-100">
+          <details class="group py-8" open>
+            <summary class="flex cursor-pointer items-center justify-between">
+              <h2 class="text-lg font-medium text-gray-900">
+                Lorem ipsum dolor sit amet consectetur adipisicing?
+              </h2>
+
+              <span class="relative ml-1.5 h-5 w-5 flex-shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </span>
+            </summary>
+
+            <p class="mt-4 leading-relaxed text-gray-700">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+              veritatis molestias culpa in, recusandae laboriosam neque aliquid
+              libero nesciunt voluptate dicta quo officiis explicabo
+              consequuntur distinctio corporis earum similique!
+            </p>
+          </details>
+
+          <details class="group py-8">
+            <summary class="flex cursor-pointer items-center justify-between">
+              <h2 class="text-lg font-medium text-gray-900">
+                Lorem ipsum dolor sit amet consectetur adipisicing?
+              </h2>
+
+              <span class="relative ml-1.5 h-5 w-5 flex-shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </span>
+            </summary>
+
+            <p class="mt-4 leading-relaxed text-gray-700">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+              veritatis molestias culpa in, recusandae laboriosam neque aliquid
+              libero nesciunt voluptate dicta quo officiis explicabo
+              consequuntur distinctio corporis earum similique!
+            </p>
+          </details>
+        </div>
+      </div>
+
+      <div className="relative w-1/3 h-1/2 hidden md:block lg:block">
+        <img
+          alt="Welcome"
+          src={faqsImage}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default FAQs;
