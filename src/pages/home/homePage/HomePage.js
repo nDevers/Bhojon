889d41a05884b90eea36d35349de60c0f7e3fb07
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-11-07 15:57:45 +0600

    cookie policy in home page

--- src/pages/home/homePage/HomePage.js
+++ src/pages/home/homePage/HomePage.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React from "react";
 import { useEffect, useState } from "react";
 import LoadingSpinner from "../../../components/LoadingSpinner";
-import useWebsiteTitle from '../../../hooks/useWebsiteTitle';
-import Banner from './Banner';
-import Features from './Features';
-import Hero from './Hero';
-import Products from './Products';
-import Services from './Services';
+import useWebsiteTitle from "../../../hooks/useWebsiteTitle";
+import CookiePolicy from "./CookiePolicy";
+import Banner from "./Banner";
+import Features from "./Features";
+import Hero from "./Hero";
+import Products from "./Products";
+import Services from "./Services";
 
 const HomePage = () => {
-    // set website title
-    useWebsiteTitle('Home')
+  // set website title
+  useWebsiteTitle("Home");
 
-    return (
-        <div>
-            <Banner />
-            <Products />
-            <Services />
-        </div >
-    );
+  return (
+    <div>
+      <Banner />
+      <Products />
+      <Services />
+    </div>
+  );
 };
 
 export default HomePage;
